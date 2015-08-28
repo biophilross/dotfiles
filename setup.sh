9fa40e5ed176fc8476ff46549f0b154c47430dc5
Phil Ross <philippross369@gmail.com>
2015-08-28 12:21:23 -0400

    added vim syntax files and vim directory

--- setup.sh
+++ setup.sh
@@ -15,32 +15,32 @@ system=$1
 
 ########## Variables
 
-dir="$HOME/.dotfiles"                    # dotfiles directory
-olddir="$HOME/.dotfiles_old"             # old dotfiles backup directory
-files=$(ls ./$system)                    # list of files/folders to symlink in homedir
+dir="$HOME/.dotfiles/"                    # dotfiles directory
+olddir="$HOME/.dotfiles_old/"             # old dotfiles backup directory
+files=$(ls ./$system)                     # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old
 echo -n "Creating $olddir for backup of any existing dotfiles in $HOME ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "done"
 
 # create dotfiles
 echo -n "Changing to the $dir directory ..."
-mkdir -p $dir
+mkdir -p "$dir"
 echo "done"
 
 # move any existing dotfiles in $HOME to dotfiles_old directory, then create symlinks from $HOME to any files here
 for file in $files; do
     echo "Moving any existing dotfiles from $HOME to $olddir"
-    if [[ -a $HOME/.$file ]]; then
-        mv $HOME/.$file $olddir
+    if [[ -e "$HOME/.$file" ]]; then
+        mv "$HOME/.$file $olddir"
     fi
     echo "Moving files to $dir"
-    mv $file $dir
+    mv "$file" "$dir"
     echo "Symlinking to $HOME"
-    ln -s $dir/$file $HOME/.$file
+    ln -s "$dir/$file" "$HOME/.$file"
 done
 
 mkdir "$HOME/.vim/un"

cc8ab61ff2c59e245f787d51f8d21916ef4e5a37
Phil Ross <philippross369@gmail.com>
2015-12-26 20:42:52 -0500

    added brew file to osx setup

---
+++ osx/start_brewing.sh
@@ -0,0 +1,49 @@
+# Quick look plugins
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webpquicklook
+brew cask install suspicious-package
+
+# Apps
+brew cask install alfred
+brew cask install asepsis
+brew cask install appcleaner
+brew cask install caffeine
+brew cask install cheatsheet
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install flux
+brew cask install 1password
+brew cask install pdftk
+brew cask install spectacle
+brew cask install superduper
+brew cask install totalfinder
+brew cask install transmission
+brew cask install vlc
+brew cask install dockertoolbox
+brew cask install rstudio
+brew cask install filezilla
+brew cask install osxfuse
+
+# Command line
+brew install vim
+brew install archey
+brew install r
+brew install pandoc
+brew install tmux
+brew install sshfs
+brew install tree
+brew install wget
+brew install htop-osx
+brew install igv
+brew install igvtools
+brew install dos2unix
+brew install coreutils
+brew install parallel
+brew install littler
